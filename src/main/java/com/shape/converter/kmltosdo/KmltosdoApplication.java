a0717f5576834c4897bbec30a3480dc717cd2759
cgonul <caglar.gonul.001@gmail.com>
2019-07-27 21:37:10 +0300

    Initial commit.

---
+++ src/main/java/com/shape/converter/kmltosdo/KmltosdoApplication.java
@@ -0,0 +1,13 @@
+package com.shape.converter.kmltosdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmltosdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmltosdoApplication.class, args);
+	}
+
+}
